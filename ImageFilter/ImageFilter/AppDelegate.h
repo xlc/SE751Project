1ba3445a9b43404de4042be7b752628719e75c9d
xche185 <xche185@faf831e5-a2bb-0410-845a-b99f761cfef8>
2012-03-22 05:24:57 +0000

    create ImageFilter project
    git-svn-id: https://svn.ece.auckland.ac.nz/svn/se751-2012-g2/trunk@3 faf831e5-a2bb-0410-845a-b99f761cfef8

---
+++ ImageFilter/ImageFilter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageFilter
+//
+//  Created by Xiliang Chen on 12-3-22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ImageFilter/ImageFilter/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  ImageFilter
+//
+//  Created by Xiliang Chen on 12-3-22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
