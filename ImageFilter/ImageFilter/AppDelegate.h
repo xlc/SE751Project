401d9a6195408ee03e55a0bdec9af24ccdc49f8e
xche185 <xche185@faf831e5-a2bb-0410-845a-b99f761cfef8>
2012-03-22 06:54:48 +0000

    basic GUI implemented, can open and show image
    git-svn-id: https://svn.ece.auckland.ac.nz/svn/se751-2012-g2/trunk@5 faf831e5-a2bb-0410-845a-b99f761cfef8

--- ImageFilter/ImageFilter/AppDelegate.h
+++ ImageFilter/ImageFilter/AppDelegate.h
@@ -11,5 +11,15 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSDrawer *drawer;
+@property (weak) IBOutlet NSTextField *imagePathField;
+@property (weak) IBOutlet NSImageView *_imgView;
+
+- (IBAction)openImage:(id)sender;
+- (IBAction)selectImage:(id)sender;
+- (IBAction)toggleDrawer:(id)sender;
+- (IBAction)gcdGrayscaleApply:(id)sender;
+- (IBAction)tpGrayscaleApply:(id)sender;
+
 
 @end
--- ImageFilter/ImageFilter/AppDelegate.m
+++ ImageFilter/ImageFilter/AppDelegate.m
@@ -8,13 +8,67 @@
 
 #import "AppDelegate.h"
 
+@interface AppDelegate ()
+
+- (void)handleImage:(NSString *)imagePath;
+
+@end
+
 @implementation AppDelegate
 
 @synthesize window = _window;
+@synthesize drawer = _drawer;
+@synthesize imagePathField = _imagePathField;
+@synthesize _imgView;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [_drawer open];
+}
+
+
+#pragma mark - IBAction
+
+- (IBAction)openImage:(id)sender {
+    [self handleImage:_imagePathField.stringValue];
+}
+
+- (IBAction)selectImage:(id)sender {
+    
+    NSOpenPanel* openPanel = [NSOpenPanel openPanel];
+    [openPanel setCanChooseFiles:YES];
+    [openPanel setCanChooseDirectories:NO];
+    [openPanel setAllowsMultipleSelection:NO];
+    [openPanel beginSheetModalForWindow:_window completionHandler:^(NSInteger result) {
+        if (result == NSFileHandlingPanelOKButton) {
+            _imagePathField.stringValue = [[openPanel URL] relativePath];
+            [self openImage:nil];
+        }
+    }];
+}
+
+- (IBAction)toggleDrawer:(id)sender {
+    [_drawer toggle:self];
+}
+
+- (IBAction)gcdGrayscaleApply:(id)sender {
+    
+}
+
+- (IBAction)tpGrayscaleApply:(id)sender {
+}
+
+#pragma mark -
+
+- (void)handleImage:(NSString *)imagePath {
+    NSString *path = _imagePathField.stringValue;
+    
+    NSImage *image = [[NSImage alloc] initWithContentsOfFile:path];
+    if (!image) {
+        [[NSAlert alertWithMessageText:@"Invalid image path" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"File '%@' is not a valid image", _imagePathField.stringValue] beginSheetModalForWindow:_window modalDelegate:nil didEndSelector:NULL contextInfo:NULL];
+    } else {
+        _imgView.image = image;
+    }
 }
 
 @end
